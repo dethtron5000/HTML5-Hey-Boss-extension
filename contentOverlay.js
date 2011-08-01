b77a6d05a8224a6ac1a207ab56a5820d3dabad9c
peter olson <peter@overtype.com>
2011-08-01 18:31:29 -0400

    options working, adding pages to array

--- contentOverlay.js
+++ contentOverlay.js
@@ -1,7 +1,43 @@
-$(document).ready(function() {
-	z = document.createElement('div');
-	z.className = "wrapper";
 
-	$(z).insertBefore($('body :first'));	
+
+$(document).ready(function() {
+	pages = ['bar-graph.html','pie-graph.html']
+	
+	         
+
+	
+	var kickoff = function(prefs) {
+		console.log('inserted');
+		console.log(prefs);
+		if(prefs.defaultScreen =='random') {
+		 docSrc = fetchRandom(pages);
+		} else {
+			docSrc = prefs.defaultScreen;
+		}
+		realsource = chrome.extension.getURL('bosspages/'+docSrc);
+		
+		z = document.createElement('iframe');
+		z.className = "wrapper";
+		z.src = realsource;
+
+		$(z).insertBefore($('body :first'));			
+	};
+
+	
+	
+	var fetchRandom = function(pageArray) {
+		console.log()
+		rnd = (pageArray.length - 1) * Math.random();
+		rounded = Math.round(rnd);
+		console.log(rounded);
+		return pageArray[rounded];
+	};
+	
+	chrome.extension.sendRequest(
+			{reqKey: "fetchPrefs"}, 
+			function(response) {
+				 prefs = response.preferences;
+				 kickoff(prefs);
+			});	
 }
 );
