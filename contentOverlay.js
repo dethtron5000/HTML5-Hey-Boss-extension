84adc3485e74aaa69fe7690337aec8aa0740433e
Scott H <shelgeson@broadway.com>
2011-08-01 20:15:34 -0400

    Changes the padding on the iframe to be nicer

--- contentOverlay.js
+++ contentOverlay.js
@@ -26,24 +26,48 @@ $(document).ready(function() {
 		 
 		$(z).insertBefore($('body :first'));
 	
-		x = document.createElement('div');
-		x.className = "divwrapper";
-		x.innerHTML = "<input type='button' value='open' id='openButton'/><input type='button' value='close' id='closeButton'/>";
-		$(x).insertBefore($('body :first'));		
+		x = $('<div class="divwrapper">');
+/*		x.className = "divwrapper";
+		x.innerHTML = "<input type='button' value='open' id='openButton'/><input type='button' value='close' id='closeButton'/>";*/
 
-		$('.divwrapper').append(speechform);
+        var openButton = $("<input type='button' value='open' id='openButton'/>");
+        var closeButton = $("<input type='button' value='close' id='closeButton'/>");
+        
+        x.append(openButton);
+        x.append(closeButton);                
+        
+        x.css('height', '50px');
+        x.css('width', '100%');        
+        x.css('zIndex','9999');		
+        x.css('position','absolute');	        
+        closeButton.css('zIndex','9999');		
+        closeButton.css('position','absolute');	        
+
+        var thisbody = $('body');
+        var temp = 42;
+		$('body').prepend(x);		
+
+        var divwrapper = $('.divwrapper');
+		divwrapper.append(speechform);
 
         $('#main-input').bind('webkitspeechchange', function() { logWords(this.value) });
         $('#main-input').bind('speechchange', function() { logWords(this.value) });			
 		
 		$('input[id=openButton]').click(openIframe);
 		$('input[id=closeButton]').click(closeIframe);
+
 	};
 
 	
 	var openIframe = function(e) {
 		console.log($('input[id=mainIframe]'));
 		$('iframe[id=mainIframe]').css('height','1500px');
+		$('iframe[id=mainIframe]').css('backgroundColor','white');
+		$('iframe[id=mainIframe]').css('zIndex','9998');		
+		$('iframe[id=mainIframe]').css('display','block');						
+		$('iframe[id=mainIframe]').css('position','absolute');										
+		$('iframe[id=mainIframe]').css('top','0');										
+		$('iframe[id=mainIframe]').css('left','0');			
 		//$('iframe[id=mainIframe]').attr('height','100%');
 	};
 	
